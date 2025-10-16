271dce4f39565284bc465d3852ee36123cbebceb
Ankit Singh <142376440+chauhanAni5959@users.noreply.github.com>
2025-10-16 16:44:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+const line = document.getElementById('line');
+let angle = 0;
+
+setInterval(() => {
+  angle = (angle + 2) % 360; // keeps angle between 0–359 (optional)
+  line.style.transform = `translate(-50%, -50%) rotate(${angle}deg)`;
+}, 20);
